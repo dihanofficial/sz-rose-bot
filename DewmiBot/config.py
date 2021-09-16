05b4841d8a9d863264fa41e7ef2759aba5628a59
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-16 16:43:56 +0530

    Update config.py

--- DewmiBot/config.py
+++ DewmiBot/config.py
@@ -28,7 +28,7 @@ DEFAULTS = {
     "REDIS_PORT": 6379,
     "REDIS_DB_FSM": 1,
     "MONGODB_URI": "localhost",
-    "MONGO_DB": "Natsuki",
+    "MONGO_DB": "DewmiBot",
     "API_PORT": 8080,
     "JOIN_CONFIRM_DURATION": "30m",
 }
